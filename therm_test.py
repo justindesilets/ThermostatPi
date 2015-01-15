837acafef522ade0506492e483fa9083630b249d
Justin Desilets <jdesilet@adobe.com>
2015-01-14 17:01:30 -0700

    Tons of work to the UI UI is coming along nicely and soon I will get to start digging into making it functional.

--- therm_test.py
+++ therm_test.py
@@ -7,8 +7,21 @@ import sys
 import thread
 import feedparser
 import os
+import urllib2
+import json
+
+
+f = urllib2.urlopen('http://api.wunderground.com/api/a21833d6474a6f82/geolookup/conditions/q/UT/Riverton.json') 
+json_string = f.read() 
+parsed_json = json.loads(json_string) 
+location = parsed_json['location']['city'] 
+temp_f = parsed_json['current_observation']['temp_f'] 
+f.close()
 
 current_temp = 72
+wu_api_key = 'a21833d6474a6f82'
+wu_state = 'UT'
+wu_city = 'Riverton'
 
 app = Flask(__name__)
 
@@ -16,12 +29,18 @@ app = Flask(__name__)
 def main_page():
 
 	templateData = {
-		'current_temperature' : current_temp
+		'current_temperature' : current_temp,
+		'current_location' : location,
+		'outside_temp' : temp_f
 	}
 	return render_template('main.html', **templateData)
 @app.route("/override")
 def override():
 	return render_template('override.html')
+
+@app.route("/schedule")
+def schedule():
+	return render_template('schedule.html')
 
 
 if __name__ == "__main__":
