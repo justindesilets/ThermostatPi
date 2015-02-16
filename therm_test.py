dd7339bd857b9317c0390c97064bb8df2df699b9
Justin Desilets <jdesilet@adobe.com>
2015-02-15 21:43:49 -0700

    Removed commented out code that wasn't needed.

--- therm_test.py
+++ therm_test.py
@@ -16,10 +16,6 @@ import glob
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-"""base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'"""
-
 sensor = W1ThermSensor()
 temperature_in_celsius = sensor.get_temperature()
 temperature_in_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
@@ -32,7 +28,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def main_page():
-	t = sensor.get_temperature(W1ThermSensor.DEGREES_F) 
+	t = sensor.get_temperature(W1ThermSensor.DEGREES_F)
 	current_temp = float("{0:.1f}".format(t))
 
 	templateData = {
@@ -51,26 +47,6 @@ def heat_schedule():
 @app.route("/cool_schedule", methods=['POST','GET'])
 def cool_schedule():
 	return render_template('cool_schedule.html')
-"""
-def read_temp_raw():
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
 
-def read_temp():
-	global temp_f
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string) / 1000.0
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
-		return temp_f
-	time.sleep(1)
-"""
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True,)
